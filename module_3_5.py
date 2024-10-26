37794ed48bcaccad240052ec5bfb6bfe48ed1396
Noraway <113176708+Noraway@users.noreply.github.com>
2024-10-26 17:17:06 +0300

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -1,4 +1,6 @@
 def get_multiplied_digits(number):
+    if not number:
+        return 1
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) > 1:
@@ -7,4 +9,31 @@ def get_multiplied_digits(number):
         return first
 
 result = get_multiplied_digits(40203)
+print(result)
+result = get_multiplied_digits(420)
+print(result)
+
+print()
+# Когда я задала вопрос преподавателю, мне сказали, что на ввод должна даваться строка, а не число.
+# Иначе 042 на ввод дает ошибку, т.к. тип int не может начинаться на 0, возникает ошибка синтаксиса.
+# С таким определением задача будет выглядеть так:
+
+def get_multiplied_digits_str(str_number):
+    number = int(str_number)
+    if not number:
+        return 1
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+result = get_multiplied_digits_str('40203')
+print(result)
+result = get_multiplied_digits_str('420')
+print(result)
+result = get_multiplied_digits_str('042')
+print(result)
+result = get_multiplied_digits_str('0042')
 print(result)
